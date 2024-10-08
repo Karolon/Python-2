e3f3ce3871fea3bd08e58f9cf3e0d65766002f6c
Karolon2 <109922689+Karolon@users.noreply.github.com>
2024-10-08 16:04:40 +0200

    WWW

--- 1.py
+++ 1.py
@@ -1,1 +1,31 @@
-file = open('sygnaly.txt', mode='r')
+file = open(file ='sygnaly.txt', mode='r')
+
+data = []
+for f in file:
+  data.append(f.strip())
+
+anser1 =[]
+for i in range(39,len(data),40):
+  anser1.append(data[i][9]) 
+  
+anser2=[]
+wordN=[]
+for d in data:
+  wordN=[]
+  for l in d:
+    wordN.append(ord(l))
+  if (max(wordN) - min(wordN)) <= 10:
+    anser2.append(d)
+    
+print(anser2)
+  
+  
+  
+  
+
+
+
+
+file = open(file='plik.txt', mode='w')
+print(anser1, file=file)
+print(len(anser2))
