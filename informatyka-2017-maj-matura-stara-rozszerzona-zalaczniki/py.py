79a03048faa8acc34fcd7d421ab8f354f26e4716
Karolon2 <109922689+Karolon@users.noreply.github.com>
2024-10-16 11:31:43 +0200

    16.10.2024

---
+++ informatyka-2017-maj-matura-stara-rozszerzona-zalaczniki/py.py
@@ -0,0 +1,41 @@
+file = open('binarne.txt')
+
+data =[]
+for f in file:
+  data.append(f.strip())
+  
+longest = ''
+amount = 0
+for d in data:
+  if len(d)%2 == 0:
+    if d[0:len(d)//2] == d[len(d)//2:]:
+      amount+=1
+      if len(d) > len(longest):
+        longest = d
+  
+print(amount, longest, len(longest))
+
+biggest_bin = 0
+biggest = 0
+for f in data:
+  b = int(f)
+  i = int(f,2)
+  if i <= 65535:
+    if biggest < i:
+      biggest=i
+      biggest_bin = b
+      
+print(biggest_bin, biggest)
+
+
+amount = 0
+shortest = '0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+for d in data:
+  for i in range(len(d)//4):
+    if int(d[4*i:4*i+4],2)>9:
+      amount+=1
+      if len(d) < len(shortest):
+        shortest = d
+      break
+  
+print(amount, shortest)
