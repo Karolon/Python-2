7f288f90ff9d3bfa65db295b788c8d4a099faa80
Karolon2 <109922689+Karolon@users.noreply.github.com>
2024-11-18 13:39:24 +0100

    lekcja

--- Podatki/py.py
+++ Podatki/py.py
@@ -1,13 +1,12 @@
 #brutto -  ubez społeczne - ubez zdrowotne - PPK pracow - zaliczka 
 
-def brutto_netto():
-  brutto = float(input())
+def brutto_netto(brutto,ppk):
   spoleczne = brutto*(0.0976+ 0.015 + 0.0245)
   zdrowotne = (brutto - spoleczne)*0.09
   PPK = 0
   PPK_p = 0
   Koszty_uzyskania = 250
-  if input('PPK? (Y/N)') == 'Y':
+  if ppk:
     PPK = brutto*0.02
     PPK_p = brutto*0.015
     
@@ -23,7 +22,7 @@ def brutto_netto():
     należny = 0
     
   netto = round(brutto - spoleczne - zdrowotne - PPK - należny, 2)
-  print(netto)
+  return (netto)
 
 def netto_brutto():
   netto = float(input())
@@ -36,7 +35,30 @@ def netto_brutto():
   #netto = 0.79*(1-0.0976- 0.015 - 0.0245)*brutto  -(250)*0.12-300
   #netto = 0.79*0.8629*brutto - 330
   #(netto + 330)/(0.79*0.8629) = brutto
-  brutto = round((netto + 330)/((1-0.09-0.12)*(1-(0.0976+0.015+0.0245))),2)
+  brutto = round((netto - 330)/((1-0.09-0.12)*(1-(0.0976+0.015+0.0245))),2)
   print(brutto)
   
+
+
+def netto_brutto2():
+  netto = float(input())
+  i = 80000
+  brutto = brutto_netto(i,True)
+  while netto != brutto:
+    if abs(netto - brutto)< 100:
+      if brutto > netto:
+        i-=0.01
+      else:
+        i+0.01
+    else:
+      if brutto > netto:
+        i-=1
+      else:
+        i+=1
+  print(i)
+  
+  
+#i = float(input())
+#ppk = bool(input('ppk? tak=1 nie = 0'))
+#print(brutto_netto(i,True))
 netto_brutto()
--- informatyka-2020-czerwiec-matura-rozszerzona-zalaczniki (3)/zad4.py
+++ informatyka-2020-czerwiec-matura-rozszerzona-zalaczniki (3)/zad4.py
@@ -2,7 +2,9 @@ file = open('pary.txt')
 
 data = []
 for f in file:
-  data.append(int(f.strip().split()[0]))
+  data.append(f.strip().split())
+
+#4.1
   
 def is_prime(n):
   if n in [0,1,2]:
@@ -14,6 +16,7 @@ def is_prime(n):
 
 anser = []
 for d in data:
+  d = int(d[0])
   if d % 2 == 0:
     biggest_difference = [0,0]
     for i in range(2,d):
@@ -25,8 +28,41 @@ for d in data:
     anser.append(str(d)+' '+str(biggest_difference[0])+' '+str(biggest_difference[1]))
       
   
-file_ans = open('zad4.txt', mode='w')
+file_ans = open('wyniki4.txt', mode='w')
+print('4.1', file=file_ans)
 for a in anser:
   print(a, file=file_ans)
   
+  
+#4.2
+
+print('4.2',file=file_ans)
+for d in data:
+  old_s = d[1][0]
+  anser = ['',0]
+  amount = 0
+  for s in d[1]:
+    if old_s == s:
+      amount += 1
+      if amount > anser[1]:
+        anser=[old_s,amount]
+    if old_s != s:
+      amount = 0
+    old_s = s
+    if anser[1] == 0:
+      anser = [d[1][0],1]
+  print(anser[0]*anser[1], anser[1], file=file_ans)
+  
+  
+#4.3
+data2 = []
+for d in data:
+  if int(d[0]) == len(d[1]):
+    data2.append(d)
+    
+print('4.3', file = file_ans)
+print(min(data2), file = file_ans)
+
+
+
 file_ans.close()
