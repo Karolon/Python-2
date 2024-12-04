9e1abee8b7cb3cd2f21157c673442414b465682b
Karolon2 <109922689+Karolon@users.noreply.github.com>
2024-12-04 11:32:41 +0100

    lekcja

--- clasy, stack/Wieza_hanoi.py
+++ clasy, stack/Wieza_hanoi.py
@@ -9,12 +9,15 @@ root.maxsize(1000,700)
 root.mainloop()
 '''
     
+import Stack as s    
+
+
 class Hanoi:
     
-  def __init__(self):
-    self.hanoi = [[],[],[]]
-    for i in range(5):
-      self.hanoi[0].append(i+1)
+  def __init__(self, size):
+    self.hanoi = [s.Stack(),s.Stack(),s.Stack()]
+    for i in range(size):
+      self.hanoi[0].PUSH(i+1)
   
     
   def move(self, v1, v2):
@@ -32,17 +35,33 @@ class Hanoi:
       b = -1
       
     if (b == -1 or a < b) and a != -1:
-      self.hanoi[v2] = [a] + self.hanoi[v2]
-      del self.hanoi[v1][0]
+      self.hanoi[v2].PUSH(a)
+      self.hanoi[v1].POP()
     else:
       print('Nielegalny ruch')
       
   def printHanoi(self):
-    print(*self.hanoi, sep='\n')
+    printer = self.hanoi[0].printStack(), self.hanoi[1].printStack(), self.hanoi[2].printStack()
+    hight = max(self.hanoi[0].SIZE(), self.hanoi[1].SIZE(), self.hanoi[2].SIZE())
+    for i in range(hight):
+      for d in printer:
+        if d:
+          if len(d) < i:
+            print('   ', end='')
+          else:
+            print(str(d[len(d)-i]) + ' '*(3-len(str(d[len(d)-i]))), end='')
+        else:
+          print('   ', end='')
+        print()
+
+  def win(self):
+    return self.hanoi[0] == [] and (self.hanoi[1] == [] or self.hanoi[2] == [])
 
 
-tower = Hanoi()
+
+size = int(input('Podaj rozmiar'))
+tower = Hanoi(size)
 tower.printHanoi()
-while not True == False:
+while not tower.win():
   tower.move(int(input()), int(input()))
   tower.printHanoi()
